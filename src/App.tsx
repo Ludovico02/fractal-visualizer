d3fa0e8d65fa507c9677099a9ec68af33da1fb35
Ludovico Gardenal <ludovico02@outlook.com>
2026-09-06 10:10:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center font-sans">
+      <h1 className="text-4xl text-phosphor">Fractal Visualizer</h1>
+    </div>
+  )
+}
